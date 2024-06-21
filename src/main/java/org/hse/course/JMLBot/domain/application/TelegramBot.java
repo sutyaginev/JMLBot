3f7505dc2e3e695959f052b4af3b0efe192f1bb8
Evgeniy <sutyaginev@gmail.com>
2024-06-21 21:16:09 +0500

    Добавлено меню у бота

--- src/main/java/org/hse/course/JMLBot/domain/application/TelegramBot.java
+++ src/main/java/org/hse/course/JMLBot/domain/application/TelegramBot.java
@@ -4,18 +4,47 @@ import lombok.extern.slf4j.Slf4j;
 import org.hse.course.JMLBot.application.datasource.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
+    static final String HELP_TEXT = """
+            Данный бот демонстрирует возможности SpringBoot
+            
+            Можно выбрать команду в меню слева или написав её
+            
+            /start показывает приветственное сообщение
+            /printpun покажет новый каламбур
+            /help покажет данное сообщение
+            """;
+
+    static final String UNKNOWN_TEXT = "Данная команда не поддерживается";
 
     public TelegramBot(BotConfig config) {
+
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "приветствие пользователя"));
+        listOfCommands.add(new BotCommand("/printpun", "новый каламбур"));
+        listOfCommands.add(new BotCommand("/addpun", "добавить каламбур"));
+        listOfCommands.add(new BotCommand("/help", "информация об использовании"));
+
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Ошибка при настройке списка команд бота: " + e.getMessage());
+        }
     }
 
     @Override
@@ -36,12 +65,17 @@ public class TelegramBot extends TelegramLongPollingBot {
             long chatId = update.getMessage().getChatId();
 
             switch (messageText) {
+
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
 
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+
                 default:
-                    sendMessage(chatId, "Данная команда не поддерживается");
+                    sendMessage(chatId, UNKNOWN_TEXT);
                     break;
             }
         }
