425ae89209dbe0abf4986d741cc81025a31d304c
Evgeniy <sutyaginev@gmail.com>
2024-06-21 17:17:25 +0500

    Initial commit

---
+++ src/main/java/org/hse/course/JMLBot/JmlBotApplication.java
@@ -0,0 +1,13 @@
+package org.hse.course.JMLBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmlBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmlBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hse/course/JMLBot/JmlBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hse.course.JMLBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmlBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
